cf8c4c727c1adc9b03ba7c342aa7d0fc6cae3203
Tema Klochko <tema@klochko.ru>
2021-03-02 19:57:43 +0700

    Optimize leetcode longest common prefix

--- leetcode/longest_common_prefix/__init__.py
+++ leetcode/longest_common_prefix/__init__.py
@@ -1,10 +1,12 @@
+from itertools import groupby
 from typing import List
 
 
 def longest_common_prefix(strings: List[str]) -> str:
     result = []
     for c in zip(*strings):
-        if len(frozenset(c)) == 1:
+        unique = groupby(c)
+        if next(unique, True) and not next(unique, False):
             result.append(c[0])
         else:
             break
